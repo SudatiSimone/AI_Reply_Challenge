3f7f561d753f8a5ad7eeee9b2a30e7fe68055f85
l.fratini <l.fratini@reply.it>
2023-03-09 17:36:28 +0100

    cell commit

---
+++ src/it/reply/model/Cell.java
@@ -0,0 +1,7 @@
+package it.reply.model;
+
+public class Cell {
+    public int row;
+    public int column;
+    public boolean isWormholeEntry;
+}
---
+++ src/it/reply/model/SnakeOut.java
@@ -0,0 +1,8 @@
+package it.reply.model;
+
+import java.util.List;
+import java.util.Map;
+
+public class SnakeOut {
+    public Map<Integer, List<Cell>> positions;
+}
